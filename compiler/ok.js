352021f7bacee96cbe8cdf1d30f3524e04302a07
Tim Cameron Ryan <id@timryan.org>
2013-11-19 17:40:58 -0500

    Move diskio to temporary file

--- compiler/ok.js
+++ compiler/ok.js
@@ -1,4 +1,7 @@
 var acorn = require('./acorn')
 var fs = require('fs')
 
+var start = process.hrtime();
 acorn.parse(fs.readFileSync(__dirname + '/acorn.js'));
+var end = process.hrtime(start);
+console.log(end);
