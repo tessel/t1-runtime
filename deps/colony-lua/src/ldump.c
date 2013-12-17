0acb9b9fc62831779b1179d90e8158c54f34c4a1
Tim Cameron Ryan <id@timryan.org>
2013-12-17 13:55:49 -0500

    ifdef for MS support

--- deps/colony-lua/src/ldump.c
+++ deps/colony-lua/src/ldump.c
@@ -17,6 +17,31 @@
 #include "lstate.h"
 #include "lundump.h"
 
+#ifndef int8_t
+typedef __int8 int8_t;
+#endif
+#ifndef int16_t
+typedef __int16 int16_t;
+#endif
+#ifndef int32_t
+typedef __int32 int32_t;
+#endif
+#ifndef int64_t
+typedef __int64 int64_t;
+#endif
+#ifndef uint8_t
+typedef unsigned __int8 uint8_t;
+#endif
+#ifndef uint16_t
+typedef unsigned __int16 uint16_t;
+#endif
+#ifndef uint32_t
+typedef unsigned __int32 uint32_t;
+#endif
+#ifndef uint64_t
+typedef unsigned __int64 uint64_t;
+#endif
+
 typedef struct {
  lua_State* L;
  lua_Writer writer;
