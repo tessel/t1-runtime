33f28dde2af391ce501186ae9dec19ea139622cd
Tim Cameron Ryan <id@timryan.org>
2013-12-18 09:47:24 -0500

    '@' for filenames, i guess

--- src/index.js
+++ src/index.js
@@ -14,9 +14,12 @@ var compile_lua = process.platform != 'win32'
   ? __dirname + '/../bin/build/Release/compile_lua'
   : __dirname + '/../bin/compile_lua_x64.exe';
 
-function toBytecode (lua, next) {
+function toBytecode (lua, f, next) {
+  next = typeof f == 'function' ? f : next;
+  f = typeof f == 'string' ? f : 'usercode.js';
+
   var bufs = [];
-  var c = spawn(compile_lua, ['//']);
+  var c = spawn(compile_lua, ['@' + f]);
   c.stdout.on('data', function (buf) {
     bufs.push(buf);
   });
