7d8666538e6eca1a8a885bc55e7eaa690b430864
Tim Cameron Ryan <id@timryan.org>
2014-01-02 11:56:30 -0500

    Allow building on Linux

--- tools/build_tools.sh
+++ tools/build_tools.sh
@@ -1,1 +1,1 @@
-gcc -o compile_lua -I../deps/lua-5.1/src -Wno-deprecated-declarations -Wno-empty-body $(find ../deps/lua-5.1/src/ ! -name "lua.c" ! -name "luac.c" -name "*.c") compile_lua.c
+gcc -o compile_lua -I../deps/lua-5.1/src -Wno-deprecated-declarations -Wno-empty-body $(find ../deps/lua-5.1/src/ ! -name "lua.c" ! -name "luac.c" -name "*.c") -lm compile_lua.c
