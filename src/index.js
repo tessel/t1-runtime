eb4dceb738bc3919c93702774a9d1d3eced125d9
Tim Cameron Ryan <id@timryan.org>
2014-01-01 20:08:27 -0500

    Check Debug folder for colony, otherwise skip

--- src/index.js
+++ src/index.js
@@ -11,12 +11,20 @@ var colonize = require('./colonize');
  */
 
 var compile_lua = process.platform != 'win32'
-  ? __dirname + '/../bin/build/Release/compile_lua'
+  ? fs.existsSync(__dirname + '/../bin/build/Release')
+    ? __dirname + '/../bin/build/Release/compile_lua'
+    : __dirname + '/../bin/build/Debug/compile_lua'
   : __dirname + '/../bin/compile_lua_x64.exe';
 
 function toBytecode (lua, f, next) {
   next = typeof f == 'function' ? f : next;
   f = typeof f == 'string' ? f : 'usercode.js';
+
+  if (!fs.existsSync(compile_lua)) {
+    console.error('WARNING: Bytecode compiler was not compiled for module "colony". Check that node-gyp is installed properly on your system and reinstall. Skipping for now...');
+    setImmediate(next, lua, 0);
+    return;
+  }
 
   var bufs = [];
   var c = spawn(compile_lua, ['@' + f]);
