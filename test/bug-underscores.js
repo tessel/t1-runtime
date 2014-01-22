cba9f334805a078a131183212c69018230ad4ba5
Tim Cameron Ryan <id@timryan.org>
2014-01-22 12:38:02 -0500

    Adds testcases for underscores

---
+++ test/bug-underscores.js
@@ -0,0 +1,37 @@
+/* test rig */ var t = 1, tmax = 3
+function ok (a, d) { console.log(a ? 'ok ' + (t++) + ' -' : 'not ok ' + (t++) + ' -', d); }
+console.log(t + '..' + tmax);
+ok(process.versions.colony, 'running in colony')
+
+
+var dlow = 6;
+dlow = 5;
+ok(dlow != 6, 'simple variable scoping');
+
+var d_high;
+d__high = 6;
+(function () {
+	var d_high;
+	d__high = 5;
+})();
+ok(d__high == 5, 'var decl underscores are escaped properly');
+
+var a_b = 'hi';
+ok(a_b == 'hi', 'var underscores');
+
+a_b += ' there';
+ok(a_b == 'hi there', 'var underscores in lvalue of assignment');
+
+ok(a_b.toUpperCase() == 'HI THERE', 'underscore in lvalue of member expression');
+
+var c_d = {};
+c_d.cool_beans = 5;
+ok(c_d['cool_beans'] == 5, 'dynamic property values')
+
+c_d.func_tastic = function () {
+  ok(true, 'underscore in member and base')
+}
+c_d.func__tastic = function () {
+	ok(false,' underscore in member and base');
+}
+c_d.func_tastic();
