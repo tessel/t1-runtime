d49114ba60439e96801b19bc891ea1ec0b96b9c3
Tim Cameron Ryan <id@timryan.org>
2014-01-30 10:45:25 -0500

    Improves for..in testcase and Array::push testcase

--- test/bug-forinarray.js
+++ test/bug-forinarray.js
@@ -1,7 +1,17 @@
+console.log('1..1')
+
 var arr = [];
 arr.hello = 'hi';
 arr.push(1, 2, 3, 4, 5);
 
+var had0 = false;
 for (var i in arr) {
-	console.log(i, '=>', arr[i]);
+	if (i == 1 && !had0) {
+		throw new Error('1 came before 0');
+	}
+	if (i == 0) {
+		had0 = true;
+	}
 }
+
+console.log('ok')
