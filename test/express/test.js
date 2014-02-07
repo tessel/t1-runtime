389ba577488c643d0c5b7cd3504a02c084aa03b7
Tim Cameron Ryan <id@timryan.org>
2014-02-06 21:34:55 -0500

    Test for Express no longer needs debug lines

--- test/express/test.js
+++ test/express/test.js
@@ -1,13 +1,8 @@
-console.log('lord');
 var express = require('express');
 
-console.log('hey');
 var app = express();
 
-console.log('ok');
-
 app.get('/', function(req, res){
-	console.log('yay', res.render)
   res.send('Hello World');
 });
 
