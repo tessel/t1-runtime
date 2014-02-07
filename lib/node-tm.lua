15204f4f52ce912cfc7500e0f87563ac4ef45120
Tim Cameron Ryan <id@timryan.org>
2014-02-07 14:46:25 -0500

    Cleans up EventEmitter. Closes tessel/beta#125

--- lib/node-tm.lua
+++ lib/node-tm.lua
@@ -409,45 +409,53 @@ global.Buffer = Buffer
 
 local EventEmitter = function (this) end
 
-((EventEmitter).prototype).listeners = (function (this, type)
-  if true then return (((this).hasOwnProperty:call((this)._events or (function () local _r = js_obj({}); (this)._events = _r; return _r; end)(), type)) and {((this)._events)[type]} or {(function () local _r = js_arr({}); ((this)._events)[type] = _r; return _r; end)()})[1]; end;
-end);
+EventEmitter.prototype.listeners = function (this, type)
+  if not this._events then
+    this._events = js_obj({})
+  end
+  if not this.hasOwnProperty:call(this._events, type) then
+    this._events[type] = js_arr({})
+  end
+  return this._events[type]
+end
 
-((EventEmitter).prototype).on = (function () local _r = (function (this, type, f)
-  if (((this)._maxListeners ~= (0)) and (this:listeners(type):push(f) > ((this)._maxListeners or (10)))) then
-  if console and console:warn(((("Possible EventEmitter memory leak detected. ") + (((this)._events)[type]).length) + (" listeners added. Use emitter.setMaxListeners() to increase limit."))) then end;
-  end;
-  if this:emit(("newListener"), type, f) then end;
-  if true then return this; end;
-end); ((EventEmitter).prototype).addListener = _r; return _r; end)();
+EventEmitter.prototype.on = function (this, type, f)
+  if this._maxListeners ~= 0 and this:listeners(type):push(f) > (this._maxListeners or 10) then
+    global.console:warn("Possible EventEmitter memory leak detected. " + this._events[type].length + " listeners added. Use emitter.setMaxListeners() to increase limit.")
+  end
+  this:emit("newListener", type, f)
+  return this
+end
 
-((EventEmitter).prototype).once = (function (this, type, f)
-  local g = g;
-  if this:on(type, (function (this, ...)
-  local g = debug.getinfo(1, 'f').func;
-  local arguments = js_arguments(...);
-  if f:call(this, (arguments)[(0)], (arguments)[(1)], (arguments)[(2)]) then end;
-  if this:removeListener(type, g)
-       then end;
-  end)) then end;
-end);
+EventEmitter.prototype.addListener = EventEmitter.prototype.on
 
-((EventEmitter).prototype).removeListener = (function (this, type, f)
-  local i = i;
-  i = nil;
-  if ((function () local _r = this:listeners(type):indexOf(f); i = _r; return _r; end)() ~= (-(1))) and this:listeners(type):splice(i, (1)) then end;
-  if true then return this; end;
-end);
+EventEmitter.prototype.once = function (this, type, f)
+  local g = function (this, ...)
+    f(this, ...)
+    this:removeListener(type, g)
+  end
+  this:on(type, g)
+end
 
-((EventEmitter).prototype).removeAllListeners = (function (this, type)
-  local k = k;
-  for k in js_pairs((this)._events) do
-  if (not type) or (type == k) and ((this)._events)[k]:splice((0), (((this)._events)[k]).length) then end;
-  end;
-  if true then return this; end;
-end);
+EventEmitter.prototype.removeListener = function (this, type, f)
+  local i = this:listeners(type):indexOf(f);
+  if i ~= -1 then
+    this:emit("removeListener", type, f)
+    this.listeners(type):splice(i, 1)
+  end
+  return this
+end
 
-((EventEmitter).prototype).emit = (function (this, type, ...)
+EventEmitter.prototype.removeAllListeners = function (this, type)
+  for k in js_pairs(this._events) do
+    if not type or type == k then
+      this._events[k]:splice(0, this._events[k].length)
+    end
+  end
+  return this
+end
+
+EventEmitter.prototype.emit = function (this, type, ...)
   if not this._events or not this._events[type] then
     return
   end
@@ -459,11 +467,11 @@ end);
     fns[i](this, ...)
   end
   return #fns
-end);
+end
 
-((EventEmitter).prototype).setMaxListeners = (function (this, maxListeners)
-  (this)._maxListeners = maxListeners;
-end);
+EventEmitter.prototype.setMaxListeners = function (this, maxListeners)
+  this._maxListeners = maxListeners;
+end
 
 
 --[[
