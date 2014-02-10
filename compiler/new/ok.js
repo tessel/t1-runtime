0488edf9c6d20eabf469f6f35a89bb55d8b3c3a3
Tim Cameron Ryan <id@timryan.org>
2014-02-09 20:36:42 -0500

    Removes old "compiler" code

--- compiler/new/ok.js
+++
@@ -1,7 +0,0 @@
-var acorn = require('./acorn')
-var fs = require('fs')
-
-var start = process.hrtime();
-acorn.parse(fs.readFileSync(__dirname + '/acorn.js'));
-var end = process.hrtime(start);
-console.log(end);
