54128aa48d2a544ac00893f83420c00510fbc2e5
Tim Cameron Ryan <id@timryan.org>
2014-02-24 19:02:15 -0500

    Adds test case for tessel/beta#170

---
+++ test/suite/issue-beta-170.js
@@ -0,0 +1,7 @@
+/* test rig */ var t = 1, tmax = 2
+function ok (a, d) { console.log(a ? 'ok ' + (t++) + ' -' : 'not ok ' + (t++) + ' -', d); }
+console.log(t + '..' + tmax);
+ok(process.versions.colony, 'running in colony')
+
+ok(a, 'function is hoisted');
+function a () { }
