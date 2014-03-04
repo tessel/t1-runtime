bbdeaeac4b945b037b308f5cf4e69db9430ca6d0
Tim Cameron Ryan <id@timryan.org>
2014-03-04 17:20:21 -0500

    Adds finally {} block to try..catch test case.

--- test/suite/try.js
+++ test/suite/try.js
@@ -1,4 +1,4 @@
-/* test rig */ var t = 1, tmax = 1
+/* test rig */ var t = 1, tmax = 6
 function ok (a, d) { console.log(a ? 'ok ' + (t++) + ' -' : 'not ok ' + (t++) + ' -', d); }
 console.log(t + '..' + tmax);
 ok(process.versions.colony, 'running in colony')
@@ -8,3 +8,21 @@ try {
 	throw "BOO";
 } catch (e) { }
 ok(e == null, 'error should not escape scope of try #TODO')
+
+try {
+	try {
+	    throw 'hi';
+	} finally {
+	    console.log('ok');
+	}
+} catch (e) {
+	console.log('ok');
+} finally {
+	console.log('ok');
+}
+
+var err;
+try {
+    throw Error("ERRR");
+} catch (e) { err = e; }
+ok(err, 'error exists');
