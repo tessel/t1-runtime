56e667e41c3b1161986b5ac11f40ab6569e6bcfb
Tim Cameron Ryan <id@timryan.org>
2014-03-24 16:52:12 -0400

    Updates encoding test to work with npm test

--- test/suite/encoding.js
+++ test/suite/encoding.js
@@ -1,16 +1,19 @@
-console.log('1..2');
+/* test rig */ var t = 1, tmax = 10
+function ok (a, d) { console.log(a ? 'ok ' + (t++) + ' -' : 'not ok ' + (t++) + ' -', d); }
+console.log(t + '..' + tmax);
+ok(process.versions.colony, 'running in colony')
 
-console.log(String.fromCharCode(0x1A) == '\u001A' ? 'ok' : 'not ok');
-console.log(String.fromCharCode(0x1A) == '\x1A' ? 'ok' : 'not ok');
+ok(String.fromCharCode(0x1A) == '\u001A')
+ok(String.fromCharCode(0x1A) == '\x1A');
 
 console.log('#', String.fromCharCode(0x2603), 'is', String.fromCharCode(0x2603).length, 'bytes');
-console.log(String.fromCharCode(0x2603) == '☃');
-console.log(String.fromCharCode(0x2603) == '\u2603');
-console.log(String.fromCharCode(0x2603) != '\x26\x03');
+ok(String.fromCharCode(0x2603) == '☃');
+ok(String.fromCharCode(0x2603) == '\u2603');
+ok(String.fromCharCode(0x2603) != '\x26\x03');
 
 var poo = '💩';
 console.log('#', poo, 'is', poo.length, 'bytes');
-console.log(0xd83d == poo[0]);
-console.log(0xdca9 == poo[1]);
-console.log(poo == '\ud83d\udca9');
-console.log(poo != '\xd8\x3d\xdc\xa9');
+ok(0xd83d == poo[0]);
+ok(0xdca9 == poo[1]);
+ok(poo == '\ud83d\udca9');
+ok(poo != '\xd8\x3d\xdc\xa9');
