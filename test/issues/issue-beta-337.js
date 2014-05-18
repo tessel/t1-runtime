ff5d33a249d5fdf796f2750c2437807bc3468b11
Tim Cameron Ryan <id@timryan.org>
2014-05-17 19:31:59 -0700

    Adds test for tessel/beta#337

---
+++ test/issues/issue-beta-337.js
@@ -0,0 +1,8 @@
+function reduceBuffer(buf, start, end, fn, res) {
+    // NOTE: does not handle missing `res` like Array.prototype.reduce would
+    for (var i = start, len = end; i < len; ++i) {
+        res = fn(res, buf[i]);
+    }
+    return res;
+}
+
