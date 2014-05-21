177405e72f0057fe6953833325e8342e8a2b1dca
Jon <johnnyman727@gmail.com>
2014-05-21 10:20:54 -0700

    Fix Tim's dumbass broken test

--- test/issues/issue-beta-352.js
+++ test/issues/issue-beta-352.js
@@ -2,4 +2,4 @@ console.log('1..4');
 console.log([1,2,3].join() == '1,2,3' ? 'ok' : 'not ok');
 console.log([1,2,3].join('###') == '1###2###3' ? 'ok' : 'not ok');
 console.log([1,2,3].join(1) == '11213' ? 'ok' : 'not ok');
-console.log([1,2,3].join(null) == '1,2,3' ? 'ok' : 'not ok');
+console.log([1,2,3].join(null) == '1null2null3' ? 'ok' : 'not ok');
