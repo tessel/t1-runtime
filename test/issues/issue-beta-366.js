8fa208613b263388efbd9a5222bfffb9a1b2b609
Tim Cameron Ryan <id@timryan.org>
2014-05-26 01:29:44 -0700

    Allows parseInt to return NaN for null input radix 16. Closes tessel/beta#366

---
+++ test/issues/issue-beta-366.js
@@ -0,0 +1,6 @@
+console.log('1..1');
+parseInt(null, 16)
+console.log('#', parseInt(null, 16))
+console.log(isNaN(parseInt(null, 10)) ? 'ok' : 'not ok')
+console.log(isNaN(parseInt(null, 16)) ? 'ok' : 'not ok')
+console.log('ok')
