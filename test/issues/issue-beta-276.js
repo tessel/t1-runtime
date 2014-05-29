171b2f35ea6544debc727e3f879d3bdff67153d3
Tim Cameron Ryan <id@timryan.org>
2014-05-29 11:57:42 -0700

    Fixes tessel/beta#276. Invalid Arith fixed with new colony-lua version

---
+++ test/issues/issue-beta-276.js
@@ -0,0 +1,17 @@
+console.log('1..2')
+
+var a = new Date();
+console.log(new Date() - new Date() == 0 ? 'ok' : 'not ok')
+
+var a = new Date()
+var b = new Date()
+console.log(a - b == 0 ? 'ok' : 'not ok')
+
+var a = new Date();
+var a_val = Number(a);
+var b = {
+	valueOf: function () {
+		return a_val - 1000;
+	}
+}
+console.log(a - b == 1000 ? 'ok' : 'not ok')
