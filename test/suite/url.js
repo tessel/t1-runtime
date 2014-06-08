982b5b49d239aaae7097407a40171fae8a9f9e6f
Adam Magaluk <AdamMagaluK@gmail.com>
2014-06-08 12:00:11 -0400

    Updates to url.parse test

--- test/suite/url.js
+++ test/suite/url.js
@@ -1,4 +1,18 @@
-/* test rig */ var t = 1, tmax = 9
+var expected = { protocol: 'ws:',
+		 slashes: true,
+		 auth: 'user:pass',
+		 host: 'somedomain.com:1234',
+		 port: '1234',
+		 hostname: 'somedomain.com',
+		 hash: '#hash1',
+		 search: '?q=123',
+		 query: 'q=123',
+		 pathname: '/events',
+		 path: '/events?q=123',
+		 href: 'ws://user:pass@somedomain.com:1234/events?q=123#hash1' };
+
+
+/* test rig */ var t = 1, tmax = Object.keys(expected).length + 2;
 function ok (a, d) { console.log(a ? 'ok ' + (t++) + ' -' : 'not ok ' + (t++) + ' -', d); }
 function equal (a, b, d) { console.log((a === b) ? 'ok ' + (t++) + ' -' : 'not ok ' + (t++) + ' -', d); }
 
@@ -7,19 +21,6 @@ ok(process.versions.colony, 'running in colony')
 
 var url = require('url');
 ok(url.parse('http://tools.ietf.org/html/draft-ietf-6man-text-addr-representation-04#section-6'), 'url parses');
-
-var expected = { protocol: 'ws:',
-  slashes: true,
-  auth: 'user:pass',
-  host: 'somedomain.com:1234',
-  port: '1234',
-  hostname: 'somedomain.com',
-  hash: '#hash1',
-  search: '?q=123',
-  query: 'q=123',
-  pathname: '/events',
-  path: '/events?q=123',
-  href: 'ws://user:pass@somedomain.com:1234/events?q=123#hash1' };
 
 var actual = url.parse('ws://user:pass@somedomain.com:1234/events?q=123#hash1');
 
