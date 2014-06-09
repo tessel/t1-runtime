8eca4d09ff74c670f6348a2c8f67d9cc1a447651
Tim Cameron Ryan <id@timryan.org>
2014-06-09 14:51:13 -0700

    Implements Object.getPrototypeOf. Closes tessel/beta#451

--- test/issues/issue-beta-451.js
+++ test/issues/issue-beta-451.js
@@ -3,7 +3,9 @@ function tap (max) { tap.t = 1; console.log(tap.t + '..' + max); };
 function ok (a, d) { console.log((a ? '' : 'not ') + 'ok', tap.t++, '-', d); }
 function eq (a, b, c) { ok(a == b, String(c) + ': ' + String(a) + ' == ' + String(b)); }
 
-tap(2);
+tap(1);
 
-eq('Hello World'.replace(/\s|l*/g, ''), 'HeoWord', '/\s|l*/ replace');
-eq('Hello World'.replace(/l*/g, '_'), '_H_e__o_ _W_o_r__d_', '/l*/ replace');
+var obj = {};
+console.log("# obj.__proto__ =", obj.__proto__);
+console.log("# Object.getPrototypeOf(obj) =", Object.getPrototypeOf(obj));
+eq(obj.__proto__, Object.getPrototypeOf(obj), 'same prototype');
