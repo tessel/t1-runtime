40ecfad5fab29771c9453da55a1cf8c3077b5c5c
Evan Simpson <esimpson.rescue@gmail.com>
2014-07-16 15:26:49 -0700

    Add tests for function

--- test/suite/function.js
+++ test/suite/function.js
@@ -1,7 +1,15 @@
-/* test rig */ var t = 1, tmax = 1
+/* test rig */ var t = 1, tmax = 4
 function ok (a, d) { console.log(a ? 'ok ' + (t++) + ' -' : 'not ok ' + (t++) + ' -', d); }
 console.log(t + '..' + tmax);
 ok(process.versions.colony, 'running in colony')
 
 function a (a, b, c, d, e) { }
 ok(a.length == 5, 'function arity == 5')
+ok(new Function(), 'empty Function() constructor')
+
+try {
+  var b = new Function("a", "b", "console.log('')")
+  ok(false, 'new Function() does not throw error')
+} catch(err) {
+  ok(true, 'new Function(arg) throws error')
+}
