947d8d620d6a36be1da003737b7b770b027902fb
Tim Cameron Ryan <id@timryan.org>
2014-08-04 12:29:35 -0700

    Restructures git submodules to rely on LuaJIT.

--- tools/luajit-build.sh
+++ tools/luajit-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-cd deps/luajit
+cd deps/colony-luajit
 make HOST_CFLAGS="-DLUAJIT_COLONY -g" TARGET_CFLAGS="-DLUAJIT_COLONY -g" || true
 cd ../..
-cp deps/luajit/src/libluajit.a $1
+cp deps/colony-luajit/src/libluajit.a $1
