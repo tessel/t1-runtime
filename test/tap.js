af11ade3433142b0559f3987590ad6a506bddd76
Tim Cameron Ryan <id@timryan.org>
2014-08-11 00:33:47 -0700

    Adds simple tap test file for future issues tests.

---
+++ test/tap.js
@@ -0,0 +1,4 @@
+function tap (max) { tap.t = 1; console.log(tap.t + '..' + max); };
+tap.ok = function (a, d) { console.log((a ? '' : 'not ') + 'ok', tap.t++, '-', d); }
+tap.eq = function (a, b, c) { tap.ok(a == b, String(c) + ': ' + JSON.stringify(String(a)) + ' == ' + JSON.stringify(String(b))); }
+module.exports = tap;
