a6e719e1569d4123d6e47e99c389912c811a209d
johnnyman727 <johnnyman727@gmail.com>
2014-08-18 10:30:58 -0700

    Adds support for hasOwnProperty for numbers

--- src/colony/lua/colony-js.lua
+++ src/colony/lua/colony-js.lua
@@ -310,6 +310,10 @@ obj_proto.hasOwnProperty = function (ths, p)
     return p == 'length'
   end
 
+  if type(ths) == 'number' then
+    return false
+  end
+
   if type(ths) == 'function' then
     ths = js_func_proxy(ths)
   end
