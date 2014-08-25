4ec1d563ca2a88cd5336bba1cac6d31679817e59
jialiya <jialiya.huang0@gmail.com>
2014-08-25 14:37:51 -0700

    fixed net tests to pass for node and colony
    newline
    
    swap back from ok to eq

--- test/net/http-req-ip.js
+++ test/net/http-req-ip.js
@@ -8,16 +8,16 @@ var http = require('http');
 
 // This is a hardcoded IP for google.com.
 http.request({
-	hostname: '64.233.167.99',
-	port: 80,
-	path: '/index.html',
-	method: 'GET'
+  hostname: '64.233.167.99',
+  port: 80,
+  path: '/index.html',
+  method: 'GET'
 }, function (res) {
   tap.eq(typeof res.statusCode, 'number');
   console.log('# statusCode', res.statusCode)
   res.on('data', function (data) {
-  	console.log('# received', data.length, 'bytes');
+    console.log('# received', data.length, 'bytes');
   })
 }).on('error', function (e) {
   tap.ok(false, String(e));
-});
+}).end();
