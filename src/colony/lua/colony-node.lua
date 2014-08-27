d0d44a126a70e4fe9a75d983d900a672d1cb9a45
jialiya <jialiya.huang0@gmail.com>
2014-08-26 22:48:05 -0700

    a little closer to getting faye working

--- src/colony/lua/colony-node.lua
+++ src/colony/lua/colony-node.lua
@@ -1041,15 +1041,42 @@ package.preload.http_parser = function ()
   local mod = js_obj({
     HTTPParser = function (type)
       local obj, parser
+      local proxyobj = {
+        onHeaderField = function (field)
+          if obj.onHeaderField then
+            obj.onHeaderField(parser, field, 0, #field)
+          end
+        end,
+        onHeaderValue = function (field)
+          if obj.onHeaderValue then
+            obj.onHeaderValue(parser, field, 0, #field)
+          end
+        end,
+        onHeadersComplete = function (info)
+          if obj.onHeadersComplete then
+            print('--------- CMON TIM ---->', info.url);
+            obj.onHeadersComplete(parser, js_obj({
+              statusCode = info.status_code,
+              method = info.method,
+              url = info.url,
+            }))
+          end
+        end,
+        onMessageComplete = function ()
+          if obj.onMessageComplete then
+            obj.onMessageComplete(parser)
+          end
+        end
+      }
       if type == 'request' or type == 0 then
         obj = {}
-        parser = http_parser.new('request', obj)
+        parser = http_parser.new('request', proxyobj)
       else
         obj = {}
-        parser = http_parser.new('response', obj)
+        parser = http_parser.new('response', proxyobj)
       end
       obj.execute = function (this, data, start, len)
-        return parser.execute(data, start, len)
+        return parser:execute(data:toString(), start, len)
       end
       return obj
     end
