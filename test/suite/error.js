dadb39300f9aa5d236898bd1f5103dffa2b2b310
Nathan Vander Wilt <nate@calftrail.com>
2014-09-05 08:44:52 -0700

    add JavaScriptStackTraceApi tests

--- test/suite/error.js
+++ test/suite/error.js
@@ -1,6 +1,6 @@
 var tap = require('../tap');
 
-tap.count(22);
+tap.count(33);
 
 try {
 	throw new Error('ok')
@@ -10,6 +10,7 @@ try {
 
 
 tap.ok(Error)
+// TODO: these `.type ==` checks don't pass under v8!
 tap.ok(Error('ok').type == 'Error')
 tap.ok(new Error('ok').type == 'Error')
 tap.ok(EvalError)
@@ -30,3 +31,37 @@ tap.ok(new TypeError('ok').type == 'TypeError')
 tap.ok(URIError)
 tap.ok(URIError('ok').type == 'URIError')
 tap.ok(new URIError('ok').type == 'URIError')
+
+// stack capturing
+var err = new Error('thing');
+function CAPTURER(err, ctor) {
+  Error.captureStackTrace(err, ctor);
+}
+CAPTURER(err);
+tap.ok(/CAPTURER/.test(err.stack), "expected callsite in trace");
+CAPTURER(err, CAPTURER);
+tap.ok(!/CAPTURER/.test(err.stack), "excludable via argument");
+
+// stack formatting
+var prepErr, prepArr;
+Error.prepareStackTrace = function (err, arr) {
+  prepErr = err;
+  prepArr = arr;
+  return "my stack";
+}
+var err = Error();
+CAPTURER(err);
+tap.eq(err.stack, "my stack", "got return value");
+tap.eq(prepErr, err, "saw correct object");
+tap.ok(Array.isArray(prepArr), "got array too");
+tap.eq(prepArr[0].constructor.name, "CallSite", "correct object type");
+tap.eq(prepArr[0].getFunction(), CAPTURER, "correct function");
+tap.ok(prepArr.length > 1, "multiple callsites");
+
+// stack limiting
+Error.stackTraceLimit = 1;
+var err = Error();
+CAPTURER(err);
+tap.ok(err.stack);
+tap.eq(prepArr.length, 1, "only one callsite");
+tap.eq(prepArr[0].getFunction(), CAPTURER, "correct function");
