3f1b41083f73557f6867f9b95d7778359b7688a3
Nathan Vander Wilt <nate@calftrail.com>
2014-09-17 13:22:52 -0700

    a couple important (failing!) string tests

--- test/suite/string.js
+++ test/suite/string.js
@@ -1,6 +1,6 @@
 var tap = require('../tap');
 
-tap.count(8);
+tap.count(10);
 
 tap.ok("1234567890".substring(3, 6) == "456", 'substring 1')
 tap.ok("abc".substring(0, 0) == "", 'substring 2')
@@ -11,3 +11,5 @@ console.log("#", "a,b,c,d,e".split(/,/));
 tap.ok("a,b,c,d,e".split(/,/).length == 5, 'split (regexp)')
 tap.ok("abc".slice(0,0) == "", 'slice 1')
 tap.ok("abc".slice(0,1) == "a", 'slice 2')
+tap.ok("abc"[0] == "a", 'index 1')
+tap.ok("abc"['1'] == "b", 'index 2')
