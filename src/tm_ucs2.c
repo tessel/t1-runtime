f81dc9b943f190dded2529a5c5189abc6df3c5bc
Nathan Vander Wilt <nate@calftrail.com>
2014-09-19 15:21:05 -0700

    get string length working, yay!

--- src/tm_ucs2.c
+++ src/tm_ucs2.c
@@ -50,13 +50,35 @@ int32_t tm_ucs2_str_charat (const uint8_t* buf, ssize_t buf_len, ssize_t index)
 			: dst & 0xFFFF;
 }
 
-
-size_t tm_ucs2_str_lookup (const uint8_t* buf, size_t len, size_t ucs2_index, uint32_t* outchar)
+// convert UCS-2 index to offset in CESU-8 string
+size_t tm_ucs2_str_lookup_16to8 (const uint8_t* buf, size_t len, size_t ucs2_index)
 {
   const uint8_t* const orig_buf = buf;
   size_t ucs2_position = 0;
-	int32_t uchar = 0;
+	int32_t uchar;
 	while (len && ucs2_position < ucs2_index) {
+		ssize_t bytes_read = utf8proc_iterate(buf, len, &uchar);
+    if (uchar < 0) {
+      // "convert" ptr[0] to noncharacter
+      uchar = 0xFFFF;
+      bytes_read = 1;
+    }
+		buf += bytes_read;
+		len -= bytes_read;
+    // NOTE: this logic (*somewhat* incorrectly) won't split surrogate pair if `buf` is UTF-8 instead of CESU-8
+    //       we can't do any better here; IMO the correct solution is for colony-compiler to ensure CESU for us
+    ucs2_position += (uchar > 0xFFFF) ? 2 : 1;
+	}
+	return buf - orig_buf;
+}
+
+
+// convert offset in UTF-8/CESU-8 string to UCS-2 index [converts lengths, really]
+size_t tm_ucs2_str_lookup_8to16 (const uint8_t* buf, size_t len)
+{
+  size_t ucs2_position = 0;
+	while (len) {
+    int32_t uchar;
 		ssize_t bytes_read = utf8proc_iterate(buf, len, &uchar);
     if (uchar < 0) {
       // "convert" ptr[0] to noncharacter
@@ -67,7 +89,5 @@ size_t tm_ucs2_str_lookup (const uint8_t* buf, size_t len, size_t ucs2_index, ui
 		len -= bytes_read;
     ucs2_position += (uchar > 0xFFFF) ? 2 : 1;
 	}
-  if (outchar != NULL) *outchar = uchar;
-	return buf - orig_buf;
+	return ucs2_position;
 }
-
