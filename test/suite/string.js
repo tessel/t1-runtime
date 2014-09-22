eb678007f24a3fc9b769fee19f681c86b95859cc
Nathan Vander Wilt <nate@calftrail.com>
2014-09-22 15:42:02 -0700

    implement indexOf using ucs2_str_lookup

--- test/suite/string.js
+++ test/suite/string.js
@@ -1,6 +1,6 @@
 var tap = require('../tap');
 
-tap.count(10);
+tap.count(20);
 
 tap.ok("1234567890".substring(3, 6) == "456", 'substring 1')
 tap.ok("abc".substring(0, 0) == "", 'substring 2')
@@ -13,3 +13,16 @@ tap.ok("abc".slice(0,0) == "", 'slice 1')
 tap.ok("abc".slice(0,1) == "a", 'slice 2')
 tap.ok("abc"[0] == "a", 'index 1')
 tap.ok("abc"['1'] == "b", 'index 2')
+
+var s = "abc✨a✇def";
+tap.eq(s.indexOf('d'), 6);
+tap.eq(s.indexOf('def'), 6);
+tap.eq(s.indexOf('defg'), -1);
+tap.eq(s.indexOf('✨'), 3);
+tap.eq(s.indexOf('a',-1), 0);
+tap.eq(s.indexOf('a',4), 4);
+tap.eq(s.indexOf('a',5), -1);
+tap.eq(s.indexOf(''), 0);
+tap.eq(s.indexOf('',2), 2);
+//tap.eq(s.indexOf('',1 << 32), s.length);     // TODO: this is another can of worms!
+tap.eq(s.indexOf('',1000), s.length);
