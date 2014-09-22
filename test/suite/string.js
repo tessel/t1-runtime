8a49a511bf9548d5e6c4b61cd9c6518241e24b56
Nathan Vander Wilt <nate@calftrail.com>
2014-09-22 16:28:42 -0700

    remove apparent debug log

--- test/suite/string.js
+++ test/suite/string.js
@@ -7,7 +7,6 @@ tap.ok("abc".substring(0, 0) == "", 'substring 2')
 tap.ok("ababababab".indexOf('a') == 0, 'indexOf')
 tap.ok("ababababab".lastIndexOf('a') == 8, 'lastIndexOf')
 tap.ok("a,b,c,d,e".split(',').length == 5, 'split (string)')
-console.log("#", "a,b,c,d,e".split(/,/));
 tap.ok("a,b,c,d,e".split(/,/).length == 5, 'split (regexp)')
 tap.ok("abc".slice(0,0) == "", 'slice 1')
 tap.ok("abc".slice(0,1) == "a", 'slice 2')
