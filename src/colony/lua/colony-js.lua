tessel/t1-runtime

commit 87c4055530a4befb646abc3c160d623488c0768a

this by no means fully fixes String object, but at least fixes loop and makes .length and indexes less than it readonly

src/colony/lua/colony-js.lua
@@ -1033,14 +1033,18 @@ global.String = function (ths, str)
     -- save the primitive
     getmetatable(ths).__primitive = str;
 
-    -- set the length getter for the boxed value
+    -- setup the length property for the boxed value
+    js_define_setter(ths, 'length', function() end)
     js_define_getter(ths, 'length', function()
       return str.length
     end)
 
     -- set the boxed object properties
-    for i = 0, #str-1 do
-      ths[i] = str.charAt(str, i);
+    for i = 0, str.length-1 do
+      js_define_setter(ths, i, function() end)
+      js_define_getter(ths, i, function()
+        return str[i]
+      end)
     end
 
     -- return the object

commit 79f6dd7f2cb7036ccc94fd7b31db23cee1984f49

back away from regression in test/issues/issue-runtime-289.js caused by 87c4055

src/colony/lua/colony-js.lua
@@ -1041,10 +1041,12 @@ global.String = function (ths, str)
 
     -- set the boxed object properties
     for i = 0, str.length-1 do
-      js_define_setter(ths, i, function() end)
-      js_define_getter(ths, i, function()
-        return str[i]
-      end)
+      -- TODO: this would make properly read-only, but breaks a test relying on Object.keys…
+      --js_define_setter(ths, i, function() end)
+      --js_define_getter(ths, i, function()
+      --  return str[i]
+      --end)
+      ths[i] = str[i]
     end
 
     -- return the object