573b2fea1705cf29d9e07a48541427bb63d3415b
Nathan Vander Wilt <nate@calftrail.com>
2014-10-03 13:31:32 -0700

    give colony string stack helpers much clearer names

--- src/colony/lua_tm.c
+++ src/colony/lua_tm.c
@@ -52,7 +52,8 @@ inline static void stackDump (lua_State *L)
   printf("\n");  /* end the listing */
 }
 
-const char* colony_tolstring (lua_State* L, int index, size_t* res_len)
+// converts-in-place a value (typically an internal colony string) to external UTF-8 byte array
+const char* colony_tolutf8 (lua_State* L, int index, size_t* res_len)
 {
   size_t str_len;
   const uint8_t* str = (const uint8_t*) lua_tolstring(L, index, &str_len);
@@ -66,13 +67,13 @@ const char* colony_tolstring (lua_State* L, int index, size_t* res_len)
   return (const char*) utf8;
 }
 
-inline const char* colony_tostring (lua_State* L, int index)
+inline const char* colony_toutf8 (lua_State* L, int index)
 {
-  return colony_tolstring(L, index, NULL);
+  return colony_tolutf8(L, index, NULL);
 }
 
-
-void colony_pushlstring (lua_State* L, const char* utf8, size_t utf8_len)
+// pushes an external UTF-8 byte array onto stack as internal colony string
+void colony_pushlutf8 (lua_State* L, const char* utf8, size_t utf8_len)
 {
   const char* str;
   size_t str_len = tm_str_from_utf8((const uint8_t*) utf8, utf8_len, (const uint8_t**) &str);
@@ -80,9 +81,9 @@ void colony_pushlstring (lua_State* L, const char* utf8, size_t utf8_len)
   if (str != utf8) free((char*) str);
 }
 
-inline void colony_pushstring (lua_State* L, const char* utf8)
+inline void colony_pushutf8 (lua_State* L, const char* utf8)
 {
-  colony_pushlstring(L, utf8, strlen(utf8));
+  colony_pushlutf8(L, utf8, strlen(utf8));
 }
 
 
@@ -94,10 +95,10 @@ inline void colony_pushstring (lua_State* L, const char* utf8)
 static int l_tm_cwd(lua_State* L)
 {
   #ifdef CONFIG_PLATFORM_EMBED
-  colony_pushstring(L, "/app");
+  colony_pushutf8(L, "/app");
   #else
   char *cwd = getcwd(NULL, 0);
-  colony_pushstring(L, cwd);
+  colony_pushutf8(L, cwd);
   free(cwd);
   #endif
   return 1;
@@ -115,7 +116,7 @@ static int l_tm_log(lua_State* L)
 {
   const char level = lua_tonumber(L, 1);
   size_t buf_len = 0;
-  const char* buf = (const char*) colony_tolstring(L, 2, &buf_len);
+  const char* buf = (const char*) colony_tolutf8(L, 2, &buf_len);
   tm_log(level, buf, buf_len);
   return 0;
 }
@@ -128,7 +129,7 @@ static int l_tm_log(lua_State* L)
 
 static int l_tm_hostname_lookup (lua_State* L)
 {
-  const uint8_t *lookup = (const uint8_t *) colony_tostring(L, 1);
+  const uint8_t *lookup = (const uint8_t *) colony_toutf8(L, 1);
 
   uint32_t ip = tm_hostname_lookup(lookup);
   lua_pushnumber(L, ip);
@@ -327,7 +328,7 @@ static int l_tm_ssl_session_create (lua_State* L)
   tm_socket_t sock = (tm_socket_t) lua_tonumber(L, 2);
   const char* host_name = NULL;
   if (!lua_isnil(L, 3)) {
-    host_name = colony_tostring(L, 3);
+    host_name = colony_toutf8(L, 3);
   }
 
   int res = tm_ssl_session_create(&session, ctx, sock, host_name);
@@ -348,7 +349,7 @@ static int l_tm_ssl_session_altname (lua_State* L)
   if (altname == NULL) {
     lua_pushnil(L);
   } else {
-    colony_pushstring(L, altname);
+    colony_pushutf8(L, altname);
   }
   lua_pushnumber(L, res);
   return 2;
@@ -364,7 +365,7 @@ static int l_tm_ssl_session_cn (lua_State* L)
   if (cn == NULL) {
     lua_pushnil(L);
   } else {
-    colony_pushstring(L, cn);
+    colony_pushutf8(L, cn);
   }
   lua_pushnumber(L, res);
   return 2;
@@ -665,7 +666,7 @@ static int l_tm_buffer_tostring (lua_State *L)
   size_t offset = (int) lua_tonumber(L, 2);
   size_t endOffset = (int) lua_tonumber(L, 3);
   source += offset;
-  colony_pushlstring(L, (char *) source, endOffset-offset);
+  colony_pushlutf8(L, (char *) source, endOffset-offset);
   return 1;
 }
 
@@ -677,7 +678,7 @@ static int l_tm_buffer_tostring (lua_State *L)
 
 static int l_tm_fs_type (lua_State* L)
 {
-  const char *pathname = (const char *) colony_tostring(L, 1);
+  const char *pathname = (const char *) colony_toutf8(L, 1);
 
   #ifdef TM_FS_vfs
   int ret = tm_fs_type(tm_fs_root, pathname);
@@ -691,7 +692,7 @@ static int l_tm_fs_type (lua_State* L)
 
 static int l_tm_fs_open (lua_State* L)
 {
-  const char *pathname = (const char *) colony_tostring(L, 1);
+  const char *pathname = (const char *) colony_toutf8(L, 1);
   uint32_t flags = (uint32_t) lua_tonumber(L, 2);
   uint32_t mode = (uint32_t) lua_tonumber(L, 3);
 
@@ -775,7 +776,7 @@ static int l_tm_fs_write (lua_State* L)
 
 static int l_tm_fs_destroy (lua_State* L)
 {
-  const char *pathname = (const char *) colony_tostring(L, 1);
+  const char *pathname = (const char *) colony_toutf8(L, 1);
 
   #ifdef TM_FS_vfs
   tm_fs_ent* ent = NULL;
@@ -797,8 +798,8 @@ static int l_tm_fs_destroy (lua_State* L)
 
 static int l_tm_fs_rename (lua_State* L)
 {
-  const char *oldname = (const char *) colony_tostring(L, 1);
-  const char *newname = (const char *) colony_tostring(L, 2);
+  const char *oldname = (const char *) colony_toutf8(L, 1);
+  const char *newname = (const char *) colony_toutf8(L, 2);
 
   #ifdef TM_FS_vfs
   int ret = tm_fs_rename(tm_fs_root, oldname, newname);
@@ -844,7 +845,7 @@ static int l_tm_fs_length (lua_State* L)
 
 static int l_tm_fs_dir_create (lua_State* L)
 {
-  const char *pathname = (const char *) colony_tostring(L, 1);
+  const char *pathname = (const char *) colony_toutf8(L, 1);
 
   #ifdef TM_FS_vfs
   int ret = tm_fs_dir_create(tm_fs_root, pathname);
@@ -859,7 +860,7 @@ static int l_tm_fs_dir_create (lua_State* L)
 
 static int l_tm_fs_dir_open (lua_State* L)
 {
-  const char *pathname = (const char *) colony_tostring(L, 1);
+  const char *pathname = (const char *) colony_toutf8(L, 1);
 
   tm_fs_dir_t* dir = (tm_fs_dir_t*) lua_newuserdata(L, sizeof(tm_fs_dir_t));
 
@@ -887,7 +888,7 @@ static int l_tm_fs_dir_read (lua_State* L)
   if (pathname == NULL) {
     lua_pushnil(L);
   } else {
-    colony_pushstring(L, pathname == NULL ? "" : pathname);
+    colony_pushutf8(L, pathname == NULL ? "" : pathname);
   }
   lua_pushnumber(L, ret);
   return 2;
@@ -912,7 +913,7 @@ static int l_tm_utf8_char_encode (lua_State* L)
   if (len < 0) {
     lua_pushnil(L);
   } else {
-    colony_pushlstring(L, (const char*) buf, len);
+    colony_pushlutf8(L, (const char*) buf, len);
   };
   return 1;
 }
@@ -921,14 +922,14 @@ static int l_tm_utf8_char_encode (lua_State* L)
 static int l_tm_utf8_str_tolower (lua_State* L)
 {
   size_t buf_len = 0;
-  const uint8_t* buf = (const uint8_t*) colony_tolstring(L, 1, &buf_len);
+  const uint8_t* buf = (const uint8_t*) colony_tolutf8(L, 1, &buf_len);
 
   uint8_t* str_case = NULL;
   ssize_t len = tm_utf8_str_tolower(buf, buf_len, &str_case);
   if (len < 0) {
     lua_pushnil(L);
   } else {
-    colony_pushlstring(L, (const char*) str_case, buf_len);
+    colony_pushlutf8(L, (const char*) str_case, buf_len);
     free(str_case);
   }
   return 1;
@@ -938,7 +939,7 @@ static int l_tm_utf8_str_tolower (lua_State* L)
 static int l_tm_utf8_str_toupper (lua_State* L)
 {
   size_t buf_len = 0;
-  const uint8_t* buf = (const uint8_t*) colony_tolstring(L, 1, &buf_len);
+  const uint8_t* buf = (const uint8_t*) colony_tolutf8(L, 1, &buf_len);
 
 
   uint8_t* str_case = NULL;
@@ -946,7 +947,7 @@ static int l_tm_utf8_str_toupper (lua_State* L)
   if (len < 0) {
     lua_pushnil(L);
   } else {
-    colony_pushlstring(L, (const char*) str_case, buf_len);
+    colony_pushlutf8(L, (const char*) str_case, buf_len);
     free(str_case);
   }
   return 1;
@@ -1001,7 +1002,7 @@ static int l_tm_ucs2_str_lookup_8to16 (lua_State* L)
 static int l_tm_str_to_utf8 (lua_State* L)
 {
   size_t utf8_len;
-  const char* utf8 = colony_tolstring(L, 1, &utf8_len);
+  const char* utf8 = colony_tolutf8(L, 1, &utf8_len);
   lua_pushlstring(L, utf8, utf8_len);
   return 1;
 }
@@ -1010,7 +1011,7 @@ static int l_tm_str_from_utf8 (lua_State* L)
 {
   size_t utf8_len;
   const char* utf8 = lua_tolstring(L, 1, &utf8_len);
-  colony_pushlstring(L, utf8, utf8_len);
+  colony_pushlutf8(L, utf8, utf8_len);
   return 1;
 }
 
@@ -1022,7 +1023,7 @@ uint32_t tm__sync_gethostbyname (const char *domain);
 
 static int l_tm__sync_gethostbyname (lua_State* L)
 {
-  const char *host = colony_tostring(L, 1);
+  const char *host = colony_toutf8(L, 1);
 
   lua_pushnumber(L, tm__sync_gethostbyname(host));
   return 1;
@@ -1040,7 +1041,7 @@ static int l_tm_itoa (lua_State* L)
   tm_itoa(value, buf, radix == 0 ? 10 : radix);
   buf[255] = 0;
 
-  colony_pushstring(L, buf);
+  lua_pushstring(L, buf);
   return 1;
 }
 
@@ -1179,7 +1180,7 @@ static int l_tm_inflate_end (lua_State *L)
 
 static int l_tm_approxidate_milli (lua_State *L) 
 {
-  char* date_string = (char*)colony_tostring(L, 1);
+  char* date_string = (char*)colony_toutf8(L, 1);
 
   struct timeval tv;
   approxidate(date_string, &tv);
