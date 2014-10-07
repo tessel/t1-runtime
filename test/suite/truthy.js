fe680bf9281853331d6d94d7650010ded261592e
Tim Cameron Ryan <id@timryan.org>
2014-10-07 13:09:18 -0700

    Corrects use of "nil" in truthy.js test.

--- test/suite/truthy.js
+++ test/suite/truthy.js
@@ -5,7 +5,7 @@ tap.count(16);
 tap.ok(!(0) == true, '0 is falsy');
 tap.ok(!(false) == true, 'false is falsy');
 tap.ok(!(undefined) == true, 'undefined is falsy')
-tap.ok(!(nil) == true, 'nil is falsy')
+tap.ok(!(null) == true, 'null is falsy')
 tap.ok(!('') == true, '"" is falsy');
 tap.ok(!!([]) == true, '[] is truthy');
 tap.ok(!!("0") == true, '\"0\" is truthy');
@@ -15,7 +15,7 @@ var a;
 a = 0; tap.ok(!(a) == true, '0 is falsy');
 a = false; tap.ok(!(a) == true, 'false is falsy');
 a = undefined; tap.ok(!(a) == true, 'undefined is falsy')
-a = nil; tap.ok(!(a) == true, 'nil is falsy')
+a = null; tap.ok(!(a) == true, 'null is falsy')
 a = ''; tap.ok(!(a) == true, '"" is falsy');
 a = []; tap.ok(!!(a) == true, '[] is truthy');
 a = "0"; tap.ok(!!(a) == true, '\"0\" is truthy');
