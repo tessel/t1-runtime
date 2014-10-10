635154cd0517545e747ac652b598754440cdc653
Nathan Vander Wilt <nate@calftrail.com>
2014-10-09 17:38:41 -0700

    no more utf8proc uses

--- src/tm_str.c
+++ src/tm_str.c
@@ -1,17 +1,15 @@
-#include <utf8proc.h>
+#include <assert.h>
 
 #include "tm.h"
 
 uint32_t tm_str_codeat (const uint8_t* buf, size_t buf_len, size_t index)
 {
 	const uint8_t* ptr = buf;
-	int32_t dst = 0;
+	uint32_t dst = 0;
   index += 2;
 	while (index >= 2) {
-		ssize_t bytes_read = utf8proc_iterate(ptr, buf_len, &dst);
-		if (dst == -1) {
-			return dst;
-		}
+		size_t bytes_read = tm_utf8_decode(ptr, buf_len, &dst);
+		assert(dst != TM_UTF8_DECODE_ERROR);      // internal strings should never be malformed
 		ptr += bytes_read;
 		buf_len -= bytes_read;
 		index -= dst & 0x10000 ? 2 : 1;
@@ -26,28 +24,24 @@ uint32_t tm_str_codeat (const uint8_t* buf, size_t buf_len, size_t index)
 
 size_t tm_str_fromcode (uint32_t c, uint8_t* buf)
 {
-	return utf8proc_encode_char(c, buf);
+	return tm_utf8_encode(buf, 3, c);
 }
 
 // convert UCS-2 index to offset in CESU-8 string
 size_t tm_str_lookup_JsToLua (const uint8_t* buf, size_t len, size_t ucs2_index, size_t* seq_len)
 {
   const uint8_t* const orig_buf = buf;
-  ssize_t bytes_read;
+  size_t bytes_read;
   size_t ucs2_position = 0;
 	while (ucs2_position <= ucs2_index) {        // NOTE: we read _past_ ucs2_index to get seq_len
     if (len == 0) {
       bytes_read = 0;
       break;
     }
-    int32_t uchar;
-		bytes_read = utf8proc_iterate(buf, len, &uchar);
-    if (uchar < 0) {      // NOTE: it seems colony-compiler is sanitizing, so (currently) this shouldn't happen
-      // "replace" ptr[0]
-      uchar = 0xFFFD;
-      bytes_read = 1;
-    }
-		buf += bytes_read;
+    uint32_t uchar;
+		bytes_read = tm_utf8_decode(buf, len, &uchar);
+    assert(uchar != TM_UTF8_DECODE_ERROR);      // internal strings should never be malformed
+    buf += bytes_read;
 		len -= bytes_read;
     ucs2_position += (uchar > 0xFFFF) ? 2 : 1;
 	}
@@ -61,13 +55,9 @@ size_t tm_str_lookup_LuaToJs (const uint8_t* buf, size_t len)
 {
   size_t ucs2_position = 0;
 	while (len) {
-    int32_t uchar;
-		ssize_t bytes_read = utf8proc_iterate(buf, len, &uchar);
-    if (uchar < 0) {
-      // "convert" ptr[0] to noncharacter
-      uchar = 0xFFFF;
-      bytes_read = 1;
-    }
+    uint32_t uchar;
+		size_t bytes_read = tm_utf8_decode(buf, len, &uchar);
+    assert(uchar != TM_UTF8_DECODE_ERROR);      // internal strings should never be malformed
 		buf += bytes_read;
 		len -= bytes_read;
     ucs2_position += (uchar > 0xFFFF) ? 2 : 1;
--- src/tm_utf8.c
+++ src/tm_utf8.c
@@ -1,8 +1,64 @@
-#include <utf8proc.h>
 #include <assert.h>
 
 #include "tm.h"
 
+size_t tm_utf8_decode(const uint8_t* buf, size_t buf_len, uint32_t* uc) {
+  #define EXPECTSONE (buf[0] & 0x80 && buf[0] & (0x80 >> 1))      // proper lead byte of sequence
+  #define EXPECTS(n) (buf[0] & (0x80 >> n))                       // sequence of at least n bytes
+  #define CONTAINS(n) (buf_len > n && (buf[n] & 0xC0) == 0x80)    // valid continuation byte at n
+  if (buf_len > 0) {
+    if (EXPECTSONE) { if (CONTAINS(1)) {
+        if (EXPECTS(2)) { if (CONTAINS(2)) {
+            if (EXPECTS(3)) { if (CONTAINS(3)) {
+                if (EXPECTS(4)) {
+                  // unexpectedly long sequence for a Unicode character, fall down to below
+                } else {
+                  *uc = ((buf[0] & 0x07) << 18) | ((buf[1] & 0x3F) << 12) | ((buf[2] & 0x3F) << 6) | (buf[3] & 0x3F);
+                  return 4;
+                }
+              }
+            } else {
+              *uc = ((buf[0] & 0x0F) << 12) | ((buf[1] & 0x3F) << 6) | (buf[2] & 0x3F);
+              return 3;
+            }
+          }
+        } else {
+          *uc = ((buf[0] & 0x1F) << 6) | (buf[1] & 0x3F);
+          return 2;
+        }
+      }
+    } else {
+      *uc = buf[0];
+      return 1;
+    }
+  }
+  *uc = TM_UTF8_DECODE_ERROR;
+  return 0;
+}
+
+inline size_t tm_utf8_encode(uint8_t* buf, size_t buf_len, uint32_t uc) {
+  if (uc < 0x80 && buf_len > 0) {
+    buf[0] = uc;
+    return 1;
+  } else if (uc < 0x800 && buf_len > 1) {
+    buf[0] = 0xC0 + (uc >> 6);
+    buf[1] = 0x80 + (uc & 0x3F);
+    return 2;
+  } else if (uc < 0x10000 && buf_len > 2) {
+    buf[0] = 0xE0 + (uc >> 12);
+    buf[1] = 0x80 + ((uc >> 6) & 0x3F);
+    buf[2] = 0x80 + (uc & 0x3F);
+    return 3;
+  } else if (uc < 0x110000 && buf_len > 3) {
+    buf[0] = 0xF0 + (uc >> 18);
+    buf[1] = 0x80 + ((uc >> 12) & 0x3F);
+    buf[2] = 0x80 + ((uc >> 6) & 0x3F);
+    buf[3] = 0x80 + (uc & 0x3F);
+    return 4;
+  } else {
+    return 0;
+  }
+}
 
 #define IS_LEAD(uchar) (uchar > 0xD800 && uchar < 0xDC00)
 #define IS_TRAIL(uchar) (uchar > 0xDC00 && uchar <= 0xDFFF)
@@ -14,9 +70,9 @@ size_t tm_str_to_utf8 (const uint8_t* buf, size_t buf_len, const uint8_t ** cons
   int32_t hchar = 0;    // stores half of surrogate pair
   ssize_t buf_pos = 0;
   while (buf_pos < buf_len) {
-    int32_t uchar;
-		buf_pos += utf8proc_iterate(buf + buf_pos, buf_len - buf_pos, &uchar);
-    assert(uchar >= 0);     // internal strings should never be malformed, 0xFFFD replacement increases length
+    uint32_t uchar;
+		buf_pos += tm_utf8_decode(buf + buf_pos, buf_len - buf_pos, &uchar);
+    assert(uchar != TM_UTF8_DECODE_ERROR);     // internal strings should never be malformed, 0xFFFD replacement increases length
     // NOTE: this follows new behavior http://blog.nodejs.org/2014/06/16/openssl-and-breaking-utf-8-change/
     if (hchar) {
       if (IS_TRAIL(uchar)) {
@@ -24,7 +80,7 @@ size_t tm_str_to_utf8 (const uint8_t* buf, size_t buf_len, const uint8_t ** cons
         uchar = hchar + (uchar & 0x03FF);
       } else {
         // emit (preceding) unpaired lead as unknown character
-        utf8_len += utf8proc_encode_char(0xFFFD, utf8 + utf8_len);
+        utf8_len += tm_utf8_encode(utf8 + utf8_len, 3, 0xFFFD);
       }
       hchar = 0;
     }
@@ -32,7 +88,7 @@ size_t tm_str_to_utf8 (const uint8_t* buf, size_t buf_len, const uint8_t ** cons
       // lead surrogate — don't emit, but store
       hchar = 0x010000 + ((uchar & 0x03FF) << 10);
     } else {
-      utf8_len += utf8proc_encode_char(IS_TRAIL(uchar) ? 0xFFFD : uchar, utf8 + utf8_len);
+      utf8_len += tm_utf8_encode(utf8 + utf8_len, 4, IS_TRAIL(uchar) ? 0xFFFD : uchar);
     }
   }
   assert(!hchar);   // NOTE: we assume buf ends in '\0' to emit any mismatched lead surrogate
@@ -52,18 +108,18 @@ size_t tm_str_from_utf8 (const uint8_t* utf8, size_t utf8_len, const uint8_t ** 
   ssize_t utf8_pos = 0;
   while (utf8_pos < utf8_len) {
     assert(buf_pos + 6 < buf_len);        // bail if fudge factor was insufficiently generous
-    int32_t uchar;
-    ssize_t bytes_read = utf8proc_iterate(utf8 + utf8_pos, utf8_len - utf8_pos, &uchar);
-    if (uchar < 0) {
+    uint32_t uchar;
+    size_t bytes_read = tm_utf8_decode(utf8 + utf8_pos, utf8_len - utf8_pos, &uchar);
+    if (uchar == TM_UTF8_DECODE_ERROR) {
       bytes_read = 1;
       uchar = 0xFFFD;
     }
     if (bytes_read < 4) {
-      buf_pos += utf8proc_encode_char(uchar, buf + buf_pos);
+      buf_pos += tm_utf8_encode(buf + buf_pos, 3, uchar);
     } else {
       uchar -= 0x010000;
-      buf_pos += utf8proc_encode_char(0xD800 + (uchar >> 10), buf + buf_pos);
-      buf_pos += utf8proc_encode_char(0xDC00 + (uchar & 0x03FF), buf + buf_pos);
+      buf_pos += tm_utf8_encode(buf + buf_pos, 6, 0xD800 + (uchar >> 10));
+      buf_pos += tm_utf8_encode(buf + buf_pos, 3, 0xDC00 + (uchar & 0x03FF));
     }
     utf8_pos += bytes_read;
   }
