84c055c2719fcc417f528f40540a30893973cd1a
Tim Cameron Ryan <id@timryan.org>
2014-10-10 18:39:30 -0700

    Remove use of mbtowcs in hsregex.

--- test/suite/bug-regexreplace.js
+++ test/suite/bug-regexreplace.js
@@ -1,4 +1,4 @@
 console.log('1..1')
-var val = "aa".replace(/(..)/g, '\\$1\\$1\\$1') 
+var val = "aa".replace(/(..)/g, '\\$1\\$1\\$1')
 console.log(val == '\\aa\\aa\\aa' ? 'ok' : 'not ok')
 console.log('#', val)
