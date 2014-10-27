2aa33ad57b11b7a67b29e7c5fa11a68068397c83
Nathan Vander Wilt <nate@calftrail.com>
2014-10-27 16:44:01 -0700

    improve tcp/udp error handling a bit, see https://github.com/tessel/runtime/pull/595/files#r19432922 and https://github.com/tessel/runtime/pull/595/files#r19432502

--- src/colony/modules/net.js
+++ src/colony/modules/net.js
@@ -403,7 +403,8 @@ TCPSocket.prototype.__send = function (cb) {
     if (self._ssl) {
       ret = tm.ssl_write(self._ssl, buf, buf.length);
     } else {
-      ret = tm.tcp_write(self.socket, buf, buf.length);
+      // HACK/TODO: invert return value to match logic below (but AFAICT it used to always get -1 from this TCP path anyway??!)
+      ret = -tm.tcp_write(self.socket, buf, buf.length);
     }
 
     if (ret == null) {
