bab386a9b606528b3a6c1037ec04dea3485d5d25
Nathan Vander Wilt <nate@calftrail.com>
2014-11-07 15:43:17 -0800

    move network-dependent test into dgram suite per https://github.com/tessel/runtime/pull/622#discussion_r20036936

--- test/suite/dgram.js
+++ test/suite/dgram.js
@@ -33,3 +33,20 @@ test('basics', function (t) {
     });
   }
 });
+
+test("issue runtime #621", function (t) {
+  var net = require('net');
+  net.createServer(function (sock) {
+    var server = this;
+    sock.on('data', function (d) {
+      t.equal(d.length, 3);
+      server.close();
+      sock.end();     // (only needed under colony)
+      t.end();
+    });
+  }).listen(0, function () {
+    net.createConnection({port:this.address().port}, function () {
+        this.end(Buffer([0x00, 0xFF, 0x00]));
+    });
+  });
+});
