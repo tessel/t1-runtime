tessel/t1-runtime

commit 7e42f6fb8402ee50d612f5b889b5192ef68ebf15

Adds socket.destroy() calls to agent pool, removes listeners.

src/colony/modules/http.js
@@ -414,6 +414,7 @@ function _getPool(agent, opts) {
       socket.removeListener('close', handleDead);
       socket.removeListener('_free', handleFree);
       removeSocket(socket);
+      socket.destroy();
     });
     
     function handleDead() { removeSocket(socket); }
@@ -431,6 +432,7 @@ function _getPool(agent, opts) {
         freeSockets.push(socket);
       } else socket.end();
       removeSocket(socket);
+      socket.destroy();
     }
   }
   

src/colony/modules/net.js
@@ -418,15 +418,14 @@ TCPSocket.prototype.destroy = TCPSocket.prototype.close = function () {
     }
     self.emit('end')
     if (self.socket != null) {
-
       // if there is still data left, wait until its sent before we end
       if (self._outgoing.length || self._sending) {
         self._queueEnd = true;
       } else {
         self.__close();
-      }
-      
+      } 
     }
+    self.removeAllListeners();
   });
 };
 

src/posix/tm_net.c
@@ -160,7 +160,7 @@ int tm_tcp_readable (tm_socket_t sock)
         return 0;
     }
 
-    return FD_ISSET(sock, &readset) != 0 ? 1 : 0
+    return FD_ISSET(sock, &readset) != 0 ? 1 : 0;
 }
 
 int tm_tcp_listen (tm_socket_t sock, uint16_t port)

commit fd714e6abb718242d25a13d5c8c4a807c2038c9d

do not destroy free (i.e. intentionally idle) sockets (node 0.11.x "actually useful keep-alive" behavior broken in https://github.com/tessel/runtime/commit/7e42f6fb8402ee50d612f5b889b5192ef68ebf15)

src/colony/modules/http.js
@@ -451,9 +451,9 @@ function _getPool(agent, opts) {
         freeSockets.push(socket);
       } else {
         socket.end();
+        socket.destroy();     // TODO: why is this even necessary??!
       }
       removeSocket(socket);
-      socket.destroy();
     }
   }
   