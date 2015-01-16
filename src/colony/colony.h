f210fb56f3cecdc85c3383e428a338b0e0cb939f
Nathan Vander Wilt <nate@calftrail.com>
2015-01-16 15:05:20 -0800

    move colony_array_length helpers to live with other shared colony routines

--- src/colony/colony.h
+++ src/colony/colony.h
@@ -41,5 +41,7 @@ uint8_t* colony_tobuffer (lua_State* L, int index, size_t* buf_len);
 void colony_ipc_emit (lua_State* L, char *type, void* data, size_t size);
 int colony_isbuffer (lua_State *L, int index);
 int colony_isarray (lua_State* L, int index);
+void colony_array_length (lua_State* L, int pos);
+size_t colony_array_length_i (lua_State* L, int pos);
 
 LUALIB_API const char *colony_tolstring (lua_State *L, int idx, size_t *len);
